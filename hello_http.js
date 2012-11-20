9dbc2b167a3d6a24d72307dd084842ac9b4ac376
ed markovich <ed.markovich@gmail.com>
2012-11-20 16:16:34 -0500

    added fake passport authentication

--- hello_http.js
+++ hello_http.js
@@ -1,7 +1,20 @@
 var express = require("express");
 app = express(); 
-app.use(express.static(__dirname + '/public'));
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+ 
+//Todo: figure out what all of these actually are 
+app.use(express.cookieParser());
 app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
 
 var data_template = "";
 var fs = require('fs');
@@ -13,10 +26,82 @@ fs.readFile( __dirname + '/public/content/second.html', function (err, data) {
 });
 
 
-
 var doT = require('dot')
 var db = require('mongojs').connect('mydb', ['arguments']);
-app.get('/', function (req, res) {    
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+  
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+  , { id: 3, username: 'ed', password: 'ed', email: 'ed@example.com' }
+  ];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}  
+  
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    process.nextTick(function () {
+      
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
+));
+  
+
+  
+ app.get('/', ensureAuthenticated, function(req, res){
+  res.redirect('/read');
+});
+  
+
+  
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/content/first.html')
+}  
+  
+//TODOfigure out this failureflash  
+app.get('/login', 	
+  passport.authenticate('local', { failureRedirect: '/content/first.html', 
+								   failureFlash: false }),  
+								   function(req, res) {
+										res.redirect('/');
+								  });
+
+app.get('/read', function (req, res) {    
 	db.arguments.find({}, function(err, users) {
 		res.writeHead(200, { 'Content-Type': 'text/html', 'Trailer': 'Content-MD5' });
 	
@@ -34,14 +119,14 @@ app.get('/', function (req, res) {
 
 app.post('/add', function (req, res) {    
 	if(req.body.name.length === 0 || req.body.says.length === 0) {
-		res.redirect('/?alert=You probably want to say something here right now');
+		res.redirect('/read?alert=You probably want to say something here right now');
 		return;
 	}
 
 	db.arguments.save({name: req.body.name, says: req.body.says}, 
 		function(err, saved) {
 			if( err || !saved ) console.log("User not saved");
-			res.redirect('/');
+			res.redirect('/read');
 		});
 });
 
